ac9f80e36869909432d3114d1e746880c309ad9d
flvsantos15 <flvsantos300@gmail.com>
2024-12-12 15:24:35 -0300

    chore: update routes-driver gateway

--- src/routes/routes-driver/routes-driver.gateway.ts
+++ src/routes/routes-driver/routes-driver.gateway.ts
@@ -1,4 +1,5 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
+import { RoutesService } from '../routes.service'
 
 @WebSocketGateway({
   cors: {
@@ -6,8 +7,27 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
   }
 })
 export class RoutesDriverGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!'
+  constructor(private routesService: RoutesService) {}
+
+  @SubscribeMessage('client:new-points')
+  async handleMessage(client: any, payload: any) {
+    const { route_id } = payload
+    const route = await this.routesService.findOne(route_id)
+    // @ts-expect-error - routes has note been defined
+    const { steps } = route.directions.route[0].legs[0]
+    for (const step of steps) {
+      const { lat, lng } = step.start_location
+      client.emit(`server:new-points/${route_id}:list`, { route_id, lat, lng })
+
+      await sleep(2000)
+      const { lat: lat2, lng: lng2 } = step.end_location
+      client.emit(`server:new-points/${route_id}:list`, {
+        route_id,
+        lat: lat2,
+        lng: lng2
+      })
+    }
   }
 }
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
